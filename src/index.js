0b68b190b26c98d74dd3fc0da60b4b1bf247f6a8
EmiKusaba <eeemi615@gmail.com>
2020-01-09 14:07:58 -0600

    added all of code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,64 @@
 // import axios here
+const axios = require("axios");
 
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
+// Please note that it is normally not considered best practice to commit
+// api keys to github as it presents a security risk. It is done here only
 // for practice purposes as we are sharing the same account
+// const api_key = 'd771b19ef336ed8381def3a60b574464'
+
+// const discoverMovie = () => {
+//   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
+//   // code here
+//   console.log(axios)
+//   axios.get(url)
+//   .then(function (response) {
+//     // handle success
+//     return response.page;
+//   }).catch(function (error) {
+//     console.log(error)
+//    })
+// }
+
 const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url)
+    .then(function (response) {
+      // handle success
+      return response;
+    }).catch((error) => {
+      console.log(error);
+    }
+    )
 }
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
   // code here
+  return axios.get(url)
+    .then(function (response) {
+      // handle success
+      // return response;
+        return response.data;
+    }).catch((error) => {
+      console.log(error);
+    }
+    )
 }
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+  return axios.get(url)
+    .then(function (response) {
+        return response.data;
+    }).catch((error) => {
+        return error.response.status;
+    }
+    )
 }
 
 
@@ -26,5 +66,5 @@ const getMovieByIdFailure = () => {
 module.exports = {
   discoverMovie,
   getMovieById,
-  getMovieByIdFailure
-}
+  getMovieByIdFailure,
+};
